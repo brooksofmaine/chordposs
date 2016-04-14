fd5ba72ffd44e36965f7fa2a39f4de6368ba3248
Malcolm Brooks <brooksofmaine@gmail.com>
2016-04-14 08:19:06 -0400

    make house image respond to click

--- js/index.js
+++ js/index.js
@@ -5,5 +5,10 @@ $(document).ready(function(){
 	}, function() {
 	  $(this).find('.dropdown-menu').stop(true, true).delay(50).fadeOut(300);
 	});
-
+//handle click on house image
+console.log("above house click");
+  $('.house').click(function() {
+  	console.log("clicked");
+    $('.greeting').html("May there always be harmony in your house.");
+  });
 });
