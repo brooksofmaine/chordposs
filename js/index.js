63ddee051741e0198e11d2d2ca329a47fa15a987
Malcolm Brooks <brooksofmaine@gmail.com>
2016-04-17 08:26:14 -0400

    make house image toggle greeting on and off

--- js/index.js
+++ js/index.js
@@ -1,20 +1,23 @@
 $(document).ready(function(){
+	var greetingOn = false; //flag for the state of the greeting display
 	
+	//make the dropdowns open when hovered or clicked:
 	$('.dropdown').hover(function() {
 	  $(this).find('.dropdown-menu').stop(true, true).delay(50).fadeIn(500);
 	}, function() {
 	  $(this).find('.dropdown-menu').stop(true, true).delay(50).fadeOut(300);
 	});
 
+	//make the house image display a greeting if clicked:
 	 $('.house').click(function(){
- 	// $('.greeting').html("May there always be harmony in your house.");
- 		//$('.greeting').toggle(function(){
- 			console.log("clicked");
- 			$('.greeting').css({"display": "block"}).delay (2000).fadeOut(600);
- 			//$('.greeting').fadeOut(600);
-
- 		//$('greeting').slideUp(600).delay(1200).fadeOut(600);
- 		//$( ".greeting" ).slideUp( 300 ).delay( 800 ).fadeIn( 400 );
+ 			if (greetingOn === false) {
+ 			$('.greeting').css({"display": "block"});
+ 			greetingOn = true;
+ 		}
+ 		//if the greeting is on, fade it out if house image is clicked
+ 		else {
+ 			$('.greeting').fadeOut(480);
+ 			greetingOn = false;
+ 		}			
  	});
-
 });
