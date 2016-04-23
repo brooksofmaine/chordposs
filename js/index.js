da12305a2e9893622df0ee88cfa9ab0cd05ccd7d
Malcolm Brooks <brooksofmaine@gmail.com>
2016-04-23 13:25:14 -0400

    set dropdowns with click only; add 7th chords to Bb note

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
 	var greetingOn = false; //flag for the state of the greeting display
 
 	//make the dropdowns open when hovered or clicked:
@@ -31,5 +31,23 @@ $(document).ready(function(){
  			greetingOn = false;
  		}			
  	});
- */
+ 
+ 
+});
+*/
+$(document).ready(function(){
+	var greetingOn = false; //flag for the state of the greeting display
+	
+	//make the house image display a greeting if clicked:
+	 $('.house').click(function(){
+ 			if (greetingOn === false) {
+ 			$('.greeting').css({"display": "block"});
+ 			greetingOn = true;
+ 		}
+ 		//if the greeting is on, fade it out if house image is clicked
+ 		else {
+ 			$('.greeting').fadeOut(480);
+ 			greetingOn = false;
+ 		}			
+ 	});
 });
